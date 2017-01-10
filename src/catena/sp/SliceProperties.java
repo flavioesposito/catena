f54d0430d5c5f9d3cbeb1a3e5b43130468c1c74e
Flavio Esposito <flavio.esposito@gmail.com>
2017-01-09 23:32:11 -0600

    Update SliceProperties.java

--- src/catena/sp/SliceProperties.java
+++ src/catena/sp/SliceProperties.java
@@ -1,12 +1,5 @@
-/**
- * @copyright 2013 Computer Science Department laboratory, Boston University. 
- * All rights reserved. Permission to use, copy, modify, and distribute this software and its documentation
- * for any purpose and without fee is hereby granted, provided that the above copyright notice appear in all 
- * copies and that both the copyright notice and this permission notice appear in supporting documentation. 
- * The laboratory of the Computer Science Department at Boston University makes no 
- * representations about the suitability of this software for any purpose. 
- */
-package vinea.sp;
+
+package catena.sp;
 
 import java.util.LinkedList;
 
